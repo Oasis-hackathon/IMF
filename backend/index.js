087741e3d292b2a4409e18c9ba02de8267383ca6
vvvpiano <vvvpiano60@gmail.com>
2020-12-31 03:54:02 +0900

    feat: db 연결

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
+const env = require('./.env.json');
+const connection = mysql.createConnection({
+    host: env.db.host,
+    user: env.db.user,
+    password: env.db.password,
+    database: env.db.database
+})
+
+connection.connect( err => {
+    if (err)
+        console.log(err);
+    else
+        console.log("Connected to the MySQL server");
+});
 
 app.get('/',(req, res) => {
-    res.send('Hello');
+    connection.query('SELECT * from Users', (err, result) => {
+        res.send(result);
+    });
 })
 
 let server = app.listen(3001, () => {
